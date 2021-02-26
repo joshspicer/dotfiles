dd2fa3928e74bf44ee3c37dae63e02669df82ae0
Josh Spicer <hello@joshspicer.com>
2021-02-26 14:21:25 -0500

    Update symlnk.sh

--- symlnk.sh
+++ symlnk.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Run this script from its directory to symlink everything in arr to your ~ dir.
+# Named something cryptic so Codespaces does their default symlinking for me!
+
 declare -a arr=(".common" ".bashrc" ".zshrc" ".vimrc" ".gitconfig")
 
 for i in "${arr[@]}"
